8885656fcdfae996d80b204e83ec524311a44034
ryan <r.ong40915@gmail.com>
2023-09-30 23:24:04 -0400

    added new functionality

--- src/pytusm/pytusm.py
+++ src/pytusm/pytusm.py
@@ -1,2 +1,15 @@
 def add_one(number):
     return number + 1
+
+def get_highest_elem_freq(inList):
+    elem_max_freq = 0
+    dic = dict()
+    for elem in inList:
+        if elem in dic:
+            dic[elem] += 1
+            if dic[elem] > elem_max_freq:
+                elem_max_freq = dic[elem]
+        else:
+            dic[elem] = 1
+    
+    return elem_max_freq
